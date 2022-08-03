8b53f888169a77479c878be971ab0d5374a0cc91
Tahsin Emre <telli@tahsinemre.com>
2022-08-03 12:07:00 +0300

    teskatomodules

---
+++ lib/modules/pet/pet_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:petjoo/product/constants/dimens.dart';
+import 'package:petjoo/modules/pet/pet_viewmodel.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class PetView extends StatelessWidget {
+  final PetViewModel vm = PetViewModel();
+  PetView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    vm.getAdverts();
+    return buildList();
+  }
+
+  Widget buildList() {
+    return Observer(builder: (_) {
+      return grid();
+    });
+  }
+
+  Widget grid() {
+    return GridView.builder(
+        // controller: viewModel.scrollController,
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemCount: vm.adverts.length,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: .65,
+          crossAxisSpacing: Dimens.paddingSmall,
+          mainAxisSpacing: Dimens.paddingSmall,
+        ),
+        itemBuilder: (context, index) {
+          final data = vm.adverts[index];
+          return Text(data.id);
+        });
+  }
+}

891e62e238dd51e86bff7708c4deab7683b93804
Tahsin Emre <telli@tahsinemre.com>
2022-08-30 22:54:03 +0300

    deleteted old modules

--- lib/product/constants/routes.dart
+++ lib/product/constants/routes.dart
@@ -4,12 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:petjoo/infrastructure/model/transport_advert_model.dart';
 import 'package:petjoo/infrastructure/model/transport_reservation_model.dart';
 import 'package:petjoo/modules/home/view/welcome_view.dart';
-import 'package:petjoo/presentation/animal_advert/advert_image_input/advert_image_input_view.dart';
-import 'package:petjoo/presentation/animal_advert/advert_map_input/view/advert_map_input_view.dart';
-import 'package:petjoo/presentation/animal_advert/view/advert_detail_view.dart';
-import 'package:petjoo/presentation/animal_advert/view/advert_input_view.dart';
-import 'package:petjoo/presentation/animal_advert/view/advert_search_view.dart';
-import 'package:petjoo/presentation/animal_advert/view/advert_user_list_view.dart';
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_booking_view.dart';
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_detail_owner_view.dart';
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_detail_view.dart';
@@ -17,25 +11,12 @@ import 'package:petjoo/presentation/animal_transport/view/transport_advert_image
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_input_view.dart';
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_map_view.dart';
 import 'package:petjoo/presentation/animal_transport/view/transport_advert_shift_input_view.dart';
-import 'package:petjoo/presentation/auth/view/auth_view.dart';
-import 'package:petjoo/infrastructure/model/advert_model.dart';
-import 'package:petjoo/presentation/auth/view/forgot_password_view.dart';
-import 'package:petjoo/presentation/auth/view/login_view.dart';
-import 'package:petjoo/presentation/auth/view/register_view.dart';
-import 'package:petjoo/presentation/auth/view/tutorial_view.dart';
 import 'package:petjoo/presentation/chat/view/chat_list_view.dart';
 import 'package:petjoo/presentation/chat/view/chat_message_list_view.dart';
-import 'package:petjoo/presentation/common/profile/profile_view.dart';
-import 'package:petjoo/presentation/settings/view/about_view.dart';
-import 'package:petjoo/presentation/settings/view/account_settings_view.dart';
-import 'package:petjoo/presentation/settings/view/app_settings_view.dart';
-import 'package:petjoo/presentation/settings/view/document_detail_view.dart';
-import 'package:petjoo/presentation/settings/view/settings_view.dart';
 import 'package:petjoo/presentation/transport_reservation/view/transport_reservation_detail_view.dart';
 import 'package:petjoo/presentation/transport_reservation/view/transport_reservation_list_view.dart';
 import 'package:petjoo/presentation/transport_reservation/view/transport_reservation_request_list_view.dart';
 import 'package:petjoo/product/models/chat_model.dart';
-import 'docs.dart';
 
 class Routes {
   static const String tutorial = "/";
@@ -84,21 +65,6 @@ class Routes {
   static Route<dynamic>? onGenerateRoute(RouteSettings routeSettings) {
     late Widget page;
     switch (routeSettings.name) {
-      case tutorial:
-        page = const TutorialView();
-        break;
-      case auth:
-        page = const AuthView();
-        break;
-      case login:
-        page = const LoginView();
-        break;
-      case register:
-        page = const RegisterView();
-        break;
-      case forgotPassword:
-        page = ForgotPasswordView(routeSettings.arguments as String);
-        break;
       case navigation:
         // page = const NavigationView();
         page = WelcomeView();
@@ -113,24 +79,6 @@ class Routes {
         page = TransportReservationDetailView(
             routeSettings.arguments as TransportReservationModel);
         break;
-      case advertUserList:
-        page = const AdvertUserListView();
-        break;
-      case advertInput:
-        page = AdvertInputView(routeSettings.arguments as AdvertModel?);
-        break;
-      case advertMapInput:
-        page = AdvertMapInputView(routeSettings.arguments as AdvertModel);
-        break;
-      case advertImageInput:
-        page = AdvertImageInputView(routeSettings.arguments as AdvertModel);
-        break;
-      case advertSearch:
-        page = const AdvertSearchView();
-        break;
-      case advertDetail:
-        page = AdvertDetailView(routeSettings.arguments as AdvertModel);
-        break;
       case transportAdvertDetail:
         page = TransportAdvertDetailView(
             routeSettings.arguments as TransportAdvertModel);
@@ -138,9 +86,6 @@ class Routes {
       case transportAdvertBooking:
         page = TransportAdvertBookingView(
             routeSettings.arguments as TransportAdvertModel);
-        break;
-      case profile:
-        page = const ProfileView();
         break;
       case transportAdvertDetailOwner:
         page = const TransportAdvertDetailOwnerView();
@@ -160,24 +105,6 @@ class Routes {
       case transportAdvertMapInput:
         page = TransportAdvertMapView(
             geoPoint: routeSettings.arguments as GeoPoint);
-        break;
-      case settings:
-        page = const SettingsView();
-        break;
-      case appSettings:
-        page = const AppSettingsView();
-        break;
-      case accountSettings:
-        page = const AccountSettingsView();
-        break;
-      case termsOfUse:
-        page = DocumentDetailView(Docs.termsOfUse);
-        break;
-      case privacyPolicy:
-        page = DocumentDetailView(Docs.privacyPolicy);
-        break;
-      case about:
-        page = const AboutView();
         break;
       case chats:
         page = const ChatListView();
